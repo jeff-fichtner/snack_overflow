fba5e5427cecd76cc03b4eb4ed2584efd2dc5a9f
Jeff Fichtner <jeff.fichtner@gmail.com>
2016-09-21 09:30:10 -0500

    Write migrations

--- db/migrate/20160921091953_create_questions.rb
+++ db/migrate/20160921091953_create_questions.rb
@@ -1,4 +1,11 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :name do |t|
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.text :body, null: false
+
+      t.timestamps(null: false)
+    end
   end
 end
--- db/migrate/20160921091957_create_answers.rb
+++ db/migrate/20160921091957_create_answers.rb
@@ -1,4 +1,12 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+    create_table :answers do |t|
+      t.text :body, null: false
+      t.integer :user_id, null: false
+      t.integer :question_id, null: false
+      t.boolean :best
+
+      t.timestamps(null: false)
+    end
   end
 end
