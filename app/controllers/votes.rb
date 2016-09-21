0f220e861e25be1431806c964ffb989df4e77302
Dan Dobrick & Jeff Fichtner <pair+dandobrick.jeff-fichtner@devbootcamp.com>
2016-09-21 13:43:50 -0500

    Add vote button and behavior to question page

---
+++ app/controllers/votes.rb
@@ -0,0 +1,10 @@
+post '/votes' do
+  # validation needed
+  if !params[:question_id]
+    question_id = params[:voteable_id]
+  else
+    question_id = params[:question_id]
+  end
+  current_user.votes.create(votable_id: params[:votable_id], votable_type: params[:votable_type])
+  redirect "/questions/#{question_id}"
+end
