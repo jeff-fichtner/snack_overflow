983cc7613717756fba0d15fe050e4516959e09fa
Lucas Wright <LucasEWright@gmail.com>
2016-09-21 14:57:24 -0500

    added new question view and controller

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,3 +1,12 @@
+get '/questions' do
+  @questions = Question.all
+  erb :'questions/index'
+end
+
+get '/questions/new' do
+  erb :'questions/new'
+end
+
 get '/questions/:id' do
   @question =  Question.find(params[:id])
   @answers = @question.answers
@@ -6,7 +15,14 @@ get '/questions/:id' do
   erb :'questions/show'
 end
 
-get '/questions' do
-  @questions = Question.all
-  erb :'questions/index'
+post '/questions' do
+  # puts '============im here==================='
+  # p params
+  @question = Question.new(user_id: session[:user_id], title: params[:title], body: params[:body])
+    if @question.save
+      redirect "/questions/#{@question.id}'"
+    else
+      @errors = "Something went wrong. Please check your entry and resubmit your post."
+      erb :'/questions/new'
+    end
 end
--- app/helpers/session_helpers.rb
+++ app/helpers/session_helpers.rb
@@ -1,5 +1,17 @@
 helpers do
+
   def current_user
     @current_user ||= User.find(session[:user_id])
   end
+
+  def logged_in?
+    if session[:user_id] != nil
+    end
+  end
+
+  def owner?
+    if self.user_id == current_user.id
+    end
+  end
+
 end
