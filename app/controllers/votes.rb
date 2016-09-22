8e9f50a2d0a1e74c6e80258d6cef3dada6d39c36
Dan Dobrick <email": null>
2016-09-22 10:18:15 -0500

    Add login redirect for user and votes

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,12 +1,15 @@
 post '/votes' do
-  # validation needed
-  if !params[:question_id]
-    question_id = params[:votable_id]
+  if logged_in?
+    if !params[:question_id]
+      question_id = params[:votable_id]
+    else
+      question_id = params[:question_id]
+    end
+    if current_user.votes.where(votable_id: params[:votable_id], votable_type: params[:votable_type]).empty?
+      current_user.votes.create(votable_id: params[:votable_id], votable_type: params[:votable_type])
+    end
+    redirect "/questions/#{question_id}"
   else
-    question_id = params[:question_id]
+    redirect '/login'
   end
-  if current_user.votes.where(votable_id: params[:votable_id], votable_type: params[:votable_type]).empty?
-    current_user.votes.create(votable_id: params[:votable_id], votable_type: params[:votable_type])
-  end
-  redirect "/questions/#{question_id}"
 end
